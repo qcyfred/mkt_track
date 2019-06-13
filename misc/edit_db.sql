b9ce7a630137ac6ee49ed5f6e99da1f3f20f8e9a
qcy_gjzq <qcy_1993@126.com>
2019-06-13 10:12:11 +0800

    常用编辑db的sql

---
+++ misc/edit_db.sql
@@ -0,0 +1,8 @@
+use db_mkt_track;
+
+truncate a_index_bias;
+truncate a_index_bias_quantile;
+truncate a_share_bias;
+truncate a_share_bias_quantile;
+truncate a_share_pb_quantile;
+truncate a_share_pe_quantile;
